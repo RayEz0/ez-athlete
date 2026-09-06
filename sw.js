ac1affcd62f9878653e231626c52ec730d3c08de
Aman <159611386+RayEz0@users.noreply.github.com>
2026-09-06 20:35:48 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='ez-athlete-v2'; self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png'])))); self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
